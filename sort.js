b68e71ebe5738000b5b850e80a4b477f8c16afd1
Vinayak Dhaybar <vinayadhaybar09@gmail.com>
2026-02-02 09:19:45 +0530

    leetcode 88

--- sort.js
+++ sort.js
@@ -23,33 +23,60 @@
 
 
 
-// Merge sort 
+// Merge sort ---------------------------------------------------------
 
 
-function mergeSort(arr){
-    if(arr.length == 1) return arr
-    let mid = arr.length / 2
-    let leftSort = mergeSort(arr.slice(0, mid))
-    let rightSort = mergeSort(arr.slice(mid))
-    return merge(leftSort, rightSort)
-}
+// function mergeSort(arr){
+//     if(arr.length == 1) return arr
+//     let mid = arr.length / 2
+//     let leftSort = mergeSort(arr.slice(0, mid))
+//     let rightSort = mergeSort(arr.slice(mid))
+//     return merge(leftSort, rightSort)
+// }
 
-function merge(la, ra){
-    let tempArr = []
-    let lp = 0
-    let rp = 0
+// function merge(la, ra){
+//     let tempArr = []
+//     let lp = 0
+//     let rp = 0
     
-    while(lp < la.length && rp < ra.length){
-        if(la[lp] < ra[rp]){
-            tempArr.push(la[lp])
-            lp++
+//     while(lp < la.length && rp < ra.length){
+//         if(la[lp] < ra[rp]){
+//             tempArr.push(la[lp])
+//             lp++
+//         }else{
+//             tempArr.push(ra[rp])
+//             rp++
+//         }
+//     }
+//     return [...tempArr, ...la.slice(lp), ...ra.slice(rp) ]
+// }
+
+// console.log(mergeSort([2,5,4,9,1,6, 3]))
+
+
+//------Lettcode 88 -----------------------------------------------------
+
+
+function mergeArr(nums1, m, nums2, n){
+    
+    let p1 = m-1
+    let p2 = n-1
+    let k = nums1.length - 1
+    
+    while (p2 >= 0){
+        if(nums1[p1] > nums2[p2]){
+            nums1[k] = nums1[p1]
+            p1--
         }else{
-            tempArr.push(ra[rp])
-            rp++
+            nums1[k] = nums2[p2]
+            p2--
         }
+        k--
     }
-    return [...tempArr, ...la.slice(lp), ...ra.slice(rp) ]
+    
+    return nums1
+    
 }
 
-console.log(mergeSort([2,5,4,9,1,6, 3]))
 
+console.log(mergeArr([2,5,7,0,0,0], 3, [1, 3, 5], 3))
